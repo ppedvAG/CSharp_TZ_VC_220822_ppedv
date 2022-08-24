a3392c856e46bbb6f444770356649df2e9723246
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-08-24 16:21:57 +0200

    OOP

---
+++ KlassenbibliothekBeispiel/Lebewesen.cs
@@ -0,0 +1,47 @@
+﻿namespace KlassenbibliothekBeispiel
+{
+    public class Lebewesen
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 0)
+                    name = value;
+            }
+        }
+
+        public string Lieblingsnahrung { get; set; }
+
+        public DateTime Geburtsdatum { get; private set; }
+
+        public int Alter
+        {
+            get { return ((DateTime.Now - this.Geburtsdatum).Days / 365); }
+        }
+
+        public int Größe { get; set; }
+
+        public Lebewesen(string name, string nahrung, DateTime geburtsdatum, int größe)
+        {
+            this.name = name;
+            this.Lieblingsnahrung = nahrung;
+            this.Geburtsdatum = geburtsdatum;
+            this.Größe = größe;
+        }
+
+        public void Wachse()
+        {
+            this.Größe += 5;
+        }
+
+        public Lebewesen ProduziereNachwuchs(string kindname)
+        {
+            return new Lebewesen(kindname, "Muttermilch", DateTime.Now, 30);
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using KlassenbibliothekBeispiel;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlassenbibliothekBeispiel.Lebewesen lebewesen1;
+
+            lebewesen1 = new Lebewesen("Hugo Schmidt", "Spagetti", new DateTime(2002, 2, 12), 185);
+            Lebewesen lebewesen2 = new Lebewesen("Bello", "Knochen", new DateTime(2020, 12, 24), 50);
+
+            Console.WriteLine(lebewesen1.Name);
+            lebewesen1.Name = "Anna Schmidt";
+
+            Console.WriteLine(lebewesen1.Name);
+            Console.WriteLine(lebewesen2.Name);
+
+            Console.WriteLine(lebewesen1.Geburtsdatum);
+            Console.WriteLine(lebewesen1.Alter);
+
+            Console.WriteLine(lebewesen1.Größe);
+            lebewesen1.Wachse();
+            Console.WriteLine(lebewesen1.Größe);
+
+            Lebewesen kind = lebewesen1.ProduziereNachwuchs("Maria Schmidt");
+        }
+    }
+}
