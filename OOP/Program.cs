787cc064297fe5e0e50d4dc0f180346a8eaffd6b
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-08-24 17:07:18 +0200

    Modul 06: OOP + Lab (kommentiert)

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,25 +6,28 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            #region Modul 06: OOP
+            //Deklarierung von Lebewesen-Variablen und Instanziierung von neuen Lebewesen-Objekten per Konstruktor
             KlassenbibliothekBeispiel.Lebewesen lebewesen1;
+            lebewesen1 = new Lebewesen("Hugo Schmidt", "Pizza", new DateTime(2002, 2, 12), 185);
+            Lebewesen lebewesen2 = new Lebewesen("Bello", "Knochen", new DateTime(2020, 3, 15), 50);
 
-            lebewesen1 = new Lebewesen("Hugo Schmidt", "Spagetti", new DateTime(2002, 2, 12), 185);
-            Lebewesen lebewesen2 = new Lebewesen("Bello", "Knochen", new DateTime(2020, 12, 24), 50);
-
-            Console.WriteLine(lebewesen1.Name);
+            //Lesezugriff auf Property per Getter
+            Console.WriteLine("Lebewesen1 heißt " + lebewesen1.Name);
+            //Schreibzugriff auf Property per Setter
             lebewesen1.Name = "Anna Schmidt";
 
-            Console.WriteLine(lebewesen1.Name);
-            Console.WriteLine(lebewesen2.Name);
+            Console.WriteLine("Lebewesen1 heißt " + lebewesen1.Name);
+            Console.WriteLine("Lebewesen2 heißt " + lebewesen2.Name);
 
             Console.WriteLine(lebewesen1.Geburtsdatum);
-            Console.WriteLine(lebewesen1.Alter);
+            Console.WriteLine(lebewesen2.Alter);
 
-            Console.WriteLine(lebewesen1.Größe);
+            //Aufruf von klasseneigenen Funktionen
             lebewesen1.Wachse();
-            Console.WriteLine(lebewesen1.Größe);
-
-            Lebewesen kind = lebewesen1.ProduziereNachwuchs("Maria Schmidt");
+            lebewesen2.Wachse();
+            Lebewesen kind = lebewesen1.ProduziereNachwuchs("Maria");
+            #endregion
         }
     }
 }
