0ac9f8c41b43d8dc991a934903057c6b5500be0c
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-08-30 15:57:09 +0200

    M14: WinForms

---
+++ WinFormsBsp/MainWindow.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WinFormsBsp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_KlickMich = new System.Windows.Forms.Button();
+            this.Cbx_HakMichAb = new System.Windows.Forms.CheckBox();
+            this.Cbb_Auswahl = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Lbl_Output = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.Tbx_Input = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuesFensterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neuesDialogFensterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schließenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bearbeitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Btn_Ok = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_KlickMich
+            // 
+            this.Btn_KlickMich.Location = new System.Drawing.Point(100, 100);
+            this.Btn_KlickMich.Name = "Btn_KlickMich";
+            this.Btn_KlickMich.Size = new System.Drawing.Size(184, 72);
+            this.Btn_KlickMich.TabIndex = 0;
+            this.Btn_KlickMich.Text = "Klick mich";
+            this.Btn_KlickMich.UseVisualStyleBackColor = true;
+            this.Btn_KlickMich.Click += new System.EventHandler(this.Btn_KlickMich_Click);
+            // 
+            // Cbx_HakMichAb
+            // 
+            this.Cbx_HakMichAb.AutoSize = true;
+            this.Cbx_HakMichAb.Location = new System.Drawing.Point(108, 216);
+            this.Cbx_HakMichAb.Name = "Cbx_HakMichAb";
+            this.Cbx_HakMichAb.Size = new System.Drawing.Size(179, 36);
+            this.Cbx_HakMichAb.TabIndex = 1;
+            this.Cbx_HakMichAb.Text = "Hak mich ab";
+            this.Cbx_HakMichAb.UseVisualStyleBackColor = true;
+            // 
+            // Cbb_Auswahl
+            // 
+            this.Cbb_Auswahl.FormattingEnabled = true;
+            this.Cbb_Auswahl.Items.AddRange(new object[] {
+            "Item1",
+            "Auswahl2",
+            "Eintrag3"});
+            this.Cbb_Auswahl.Location = new System.Drawing.Point(106, 315);
+            this.Cbb_Auswahl.Name = "Cbb_Auswahl";
+            this.Cbb_Auswahl.Size = new System.Drawing.Size(242, 40);
+            this.Cbb_Auswahl.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(108, 396);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(240, 164);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Lbl_Output
+            // 
+            this.Lbl_Output.AutoSize = true;
+            this.Lbl_Output.Location = new System.Drawing.Point(100, 65);
+            this.Lbl_Output.Name = "Lbl_Output";
+            this.Lbl_Output.Size = new System.Drawing.Size(298, 32);
+            this.Lbl_Output.TabIndex = 4;
+            this.Lbl_Output.Text = "Bitte klicke auf den Button";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(569, 579);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(184, 36);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(787, 78);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(184, 36);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(57, 777);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(184, 36);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Location = new System.Drawing.Point(569, 276);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 221);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(17, 103);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(184, 36);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(18, 62);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(184, 36);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Tbx_Input
+            // 
+            this.Tbx_Input.AcceptsReturn = true;
+            this.Tbx_Input.Location = new System.Drawing.Point(423, 671);
+            this.Tbx_Input.Multiline = true;
+            this.Tbx_Input.Name = "Tbx_Input";
+            this.Tbx_Input.Size = new System.Drawing.Size(376, 198);
+            this.Tbx_Input.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.bearbeitenToolStripMenuItem,
+            this.hilfeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(974, 40);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neuToolStripMenuItem,
+            this.schließenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(90, 36);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // neuToolStripMenuItem
+            // 
+            this.neuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.neuesFensterToolStripMenuItem,
+            this.neuesDialogFensterToolStripMenuItem});
+            this.neuToolStripMenuItem.Name = "neuToolStripMenuItem";
+            this.neuToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.neuToolStripMenuItem.Text = "Neu";
+            // 
+            // neuesFensterToolStripMenuItem
+            // 
+            this.neuesFensterToolStripMenuItem.Name = "neuesFensterToolStripMenuItem";
+            this.neuesFensterToolStripMenuItem.Size = new System.Drawing.Size(379, 44);
+            this.neuesFensterToolStripMenuItem.Text = "Neues Fenster";
+            this.neuesFensterToolStripMenuItem.Click += new System.EventHandler(this.neuesFensterToolStripMenuItem_Click);
+            // 
+            // neuesDialogFensterToolStripMenuItem
+            // 
+            this.neuesDialogFensterToolStripMenuItem.Name = "neuesDialogFensterToolStripMenuItem";
+            this.neuesDialogFensterToolStripMenuItem.Size = new System.Drawing.Size(379, 44);
+            this.neuesDialogFensterToolStripMenuItem.Text = "Neues Dialog-Fenster";
+            this.neuesDialogFensterToolStripMenuItem.Click += new System.EventHandler(this.neuesDialogFensterToolStripMenuItem_Click);
+            // 
+            // schließenToolStripMenuItem
+            // 
+            this.schließenToolStripMenuItem.Name = "schließenToolStripMenuItem";
+            this.schließenToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.schließenToolStripMenuItem.Text = "Schließen";
+            this.schließenToolStripMenuItem.Click += new System.EventHandler(this.schließenToolStripMenuItem_Click);
+            // 
+            // bearbeitenToolStripMenuItem
+            // 
+            this.bearbeitenToolStripMenuItem.Name = "bearbeitenToolStripMenuItem";
+            this.bearbeitenToolStripMenuItem.Size = new System.Drawing.Size(149, 36);
+            this.bearbeitenToolStripMenuItem.Text = "Bearbeiten";
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(84, 36);
+            this.hilfeToolStripMenuItem.Text = "Hilfe";
+            // 
+            // Btn_Ok
+            // 
+            this.Btn_Ok.Location = new System.Drawing.Point(134, 856);
+            this.Btn_Ok.Name = "Btn_Ok";
+            this.Btn_Ok.Size = new System.Drawing.Size(150, 46);
+            this.Btn_Ok.TabIndex = 11;
+            this.Btn_Ok.Text = "Ok";
+            this.Btn_Ok.UseVisualStyleBackColor = true;
+            this.Btn_Ok.Click += new System.EventHandler(this.Btn_Ok_Click);
+            // 
+            // MainWindow
+            // 
+            this.AcceptButton = this.Btn_KlickMich;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(974, 929);
+            this.Controls.Add(this.Btn_Ok);
+            this.Controls.Add(this.Tbx_Input);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Lbl_Output);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Cbb_Auswahl);
+            this.Controls.Add(this.Cbx_HakMichAb);
+            this.Controls.Add(this.Btn_KlickMich);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Mein neues Fenster";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Btn_KlickMich;
+        private CheckBox Cbx_HakMichAb;
+        private ComboBox Cbb_Auswahl;
+        private ListBox listBox1;
+        private Label Lbl_Output;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private GroupBox groupBox1;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private TextBox Tbx_Input;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem dateiToolStripMenuItem;
+        private ToolStripMenuItem neuToolStripMenuItem;
+        private ToolStripMenuItem neuesFensterToolStripMenuItem;
+        private ToolStripMenuItem neuesDialogFensterToolStripMenuItem;
+        private ToolStripMenuItem schließenToolStripMenuItem;
+        private ToolStripMenuItem bearbeitenToolStripMenuItem;
+        private ToolStripMenuItem hilfeToolStripMenuItem;
+        private Button Btn_Ok;
+    }
+}
---
+++ WinFormsBsp/MainWindow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsBsp
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Cbb_Auswahl.Items.Add("Eintrag4");
+        }
+
+        private void Btn_KlickMich_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkOrange;
+
+            Btn_KlickMich.Text = "Button wurde angeklickt";
+
+            if(Cbx_HakMichAb.Checked)
+                (sender as Button).Left += 100;
+
+            if (Cbb_Auswahl.SelectedItem != null)
+                this.Text = Cbb_Auswahl.SelectedItem.ToString();
+
+            Lbl_Output.Text = Tbx_Input.Text;
+        }
+
+        private void schließenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            //Application.Exit();
+        }
+
+        private void neuesFensterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainWindow neuesFenster = new MainWindow();
+
+            neuesFenster.Text = "Dies ist ein neues Fenster";
+            neuesFenster.BackColor = Color.LightGreen;
+            neuesFenster.Btn_KlickMich.Text = "Klick mich auch";
+
+            neuesFenster.Show();
+        }
+
+        private void neuesDialogFensterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainWindow neuesFenster = new MainWindow();
+
+            neuesFenster.Text = "Dies ist ein neues Dialog-Fenster";
+            neuesFenster.BackColor = Color.LightGreen;
+            neuesFenster.Btn_KlickMich.Text = "Klick mich auch";
+
+            if (neuesFenster.ShowDialog() == DialogResult.OK)
+                this.Lbl_Output.Text = "Du hast auf OK geklickt";
+        }
+
+        private void Btn_Ok_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
